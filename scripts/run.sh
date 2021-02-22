0748eb35e3d8fae029326f773b3ea416ca6a5597
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-02-21 20:37:03 -0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,9 @@ if [[ $1 == "-d" ]]; then
 fi
 
 if [[ "$debug" == true ]]; then
-    build_type = "Debug"
+    build_type="Debug"
 else
-    build_type = "Release"
+    build_type="Release"
 fi
 
 executable_path="build/${build_type}/bin/ConsoleChess"
