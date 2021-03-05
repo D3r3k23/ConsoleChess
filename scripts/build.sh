7a521d7f306222e91c6a35f36f58830f1246ce3b
Derek Henderson <d3r3k523@gmail.com>
2021-03-05 10:39:50 -0800

    .

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,5 @@ fi
 build_dir="build/${build_type}"
 
 mkdir -p $build_dir
-cmake -DCMAKE_BUILD_TYPE=$build_type -B $build_dir
+cmake -DCMAKE_BUILD_TYPE=$build_type -S . -B $build_dir
 cmake --build $build_dir
