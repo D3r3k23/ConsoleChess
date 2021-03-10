1c82ad28f1d2ee683a55c13db0dff103d2ae0b5a
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-03-09 20:46:48 -0800

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,11 +1,25 @@
 
-cd ..
+@echo off
+
+
+if "%1" == "-d" (
+    set build_type=Debug
+) else (
+    set build_type=Release
+)
+
+set build_dir=build/%build_type%
 
 if not exist "build" mkdir build
+cmake -DCMAKE_BUILD_TYPE=%build_type% -S %cd% -B %build_dir%
+cmake --build $build_dir
 
-if "%1" == "-d"
-    cmake -DDebug=ON -B build
-else
-    cmake -DDebug=OFF -B build
+if [[ -f .vscode/c_cpp_properties.json ]]; then
+    cp ${build_dir}/compile_commands.json .vscode
+fi
+
+
+
+
 
 cmake --build build
